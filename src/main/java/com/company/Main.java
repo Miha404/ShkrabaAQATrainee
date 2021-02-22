0cac1da6ad7b80c890e5f893cc100f9e4dea5572
mikhailshkraba <mikhail.shkraba@gmail.com>
2021-02-22 15:29:01 +0300

    Created helloVyacheslav() method.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package main.java.com.company;
 
 import static main.java.com.company.ConsoleInput.scanner;
 
@@ -6,6 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         greetOrNotGreetUser();
+        helloVyacheslav();
+    }
+
+
+    private static void helloVyacheslav() {
+        System.out.println("Введите имя: ");
+        String inputName = scanner.next();
+        if (inputName.equals("Вячеслав")) System.out.println(String.format("Привет, %s", inputName));
+        else System.out.println("Нет такого имени");
     }
 
     private static void greetOrNotGreetUser() {
